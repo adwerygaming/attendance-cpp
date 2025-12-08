de15d174e4f8f9579f975f9d848fa157ab18657e
MasDepan <50104357+adwerygaming@users.noreply.github.com>
2025-12-08 20:41:07 +0700

    Refactor: Move attendance service logic to main.cpp
    - refactor(CMake): Remove AttendanceService.h/cpp from executable
    - refactor(main): Move attendance service functions into main.cpp
    - refactor(main): Include AttendanceTypes.h instead of AttendanceService.h

--- main.cpp
+++ main.cpp
@@ -1,10 +1,45 @@
 #include <iostream>
-#include "services//AttendanceService.h"
+
+#include "AttendanceTypes.h"
 using namespace std;
 
-int main() {
-    cout << "Attendance test" << endl;
+// functions
+const int attendanceMaxSize = 64;
+array<AttendanceEntry, attendanceMaxSize> attendanceList;
+int attendanceCount = 0;
 
-    AddEntry({ "25.12.1111", "john doe", time() });
-    return 0;
+// Create
+// marking new attendance
+bool AddEntry(AttendanceEntry entry) {
+    if (attendanceCount > attendanceMaxSize) {
+        return false;
+    }
+
+    attendanceList[attendanceCount++] = entry;
+    return true;
+}
+
+// Read
+// get all attendance on the list
+array<AttendanceEntry, attendanceMaxSize> GetAttendance() {
+    return attendanceList;
+}
+
+// Update
+// update existing attendance or sum like that idk
+bool UpdateAttendance(AttendanceEntry entry, int index) {
+    attendanceList[index] = entry;
+    return true;
+}
+
+// Delete
+// delete existing attendance or sum like that idk
+bool DeleteAttendance(int index) {
+    attendanceList[index] = {};
+    attendanceCount--;
+    return true;
+}
+
+int main() {
+
 }
